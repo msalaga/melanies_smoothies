952c272621af3d861308a08e69040deddac90352
msalaga <micle1999@azet.sk>
2024-05-14 16:59:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 
 # Write directly to the app
@@ -16,13 +17,13 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
 
 insert_btn = st.button("Submit Order")
-#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+fv_df = st.dataframe(data=my_dataframe, use_container_width=True)
 
 options = st.multiselect(
     "Choose smoothies",
